312e2064fe532b3bf2fdb8077092a4906aa6536a
0x78f1935 <codewars.nl@gmail.com>
2020-07-17 20:07:12 +0200

    g variables are now available in frontend

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom"; 
 import Navbar from "./navbar/index";
 
-ReactDOM.render(<Navbar/>, document.getElementById("navbar"));
+window.onload = function() {
+    ReactDOM.render(<Navbar/>, document.getElementById("navbar"));
+};
